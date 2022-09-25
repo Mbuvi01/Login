7f53600bb6bbad857c11e7dc3b8365a5656dfc7c
mbuvi01 <muasyaephraim01@gmail.com>
2022-09-25 23:37:59 +0300

    added a logout route

--- router.js
+++ router.js
@@ -10,11 +10,32 @@ const credential = {
 router.post("/login",(req,res)=>{
     if(req.body.email ==credential.email&&req.body.password==credential.password){
         req.session.user = req.body.email
-        // res.redirect("/dashboard")
-        res.end("Login Successful!")
+        res.redirect("/route/dashboard")
+        // res.end("Login Successful!")
     }else{
         res.end("Invalid Username")
     }
+})
+
+// dashboard route
+router.get("/dashboard", (req, res)=>{
+    if(req.session.user){
+        res.render("dashboard", {user:req.session.user})
+    }else{
+        res.send("Unauthorized user")
+    }
+})
+
+// logout route
+router.get("/logout",(req, res) => {
+    req.session.destroy(function(err){
+        if(err){
+            console.log(err)
+            res.send("Error")
+        }else{
+            res.render("base",{title:"Express",logout:"You've successfully logged out"})
+        }
+    })
 })
 
 module.exports = router
