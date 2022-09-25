49970f16699ddd69d9ee22c80e1b1f6a0ec1308d
mbuvi01 <muasyaephraim01@gmail.com>
2022-09-25 14:50:40 +0300

    created a path for the public and assets files

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require("express")
+const path = require("path")
 const app = express()
 
 const port = process.env.PORT||3000
 
 app.set("view engine", "ejs")
+
+// load static assets
+app.use("/static",express.static(path.join(__dirname,"public")))
+app.use("/assets",express.static(path.join(__dirname,"public/assets")))
 
 //home route
 app.get("/",(req, res) => {res.render("base", {title:"Login System"})})
